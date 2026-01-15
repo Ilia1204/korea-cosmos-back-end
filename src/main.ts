85cdc634f59e276c4ac31aa7b61546bd299f20e2
petrovil <lower013634@gmail.com>
2026-01-15 11:00:00 +0400

    fix: minor fixes and improvements across backend modules

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,16 @@ async function bootstrap() {
 	app.setGlobalPrefix('api')
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['https://10ed-103-109-244-3.ngrok-free.app'],
-		// origin: ['https://fd20-176-116-138-110.ngrok-free.app'],
-		// origin: ['http://localhost:3000'],
+		origin: [
+			'http://localhost:4200',
+			'http://192.168.1.138:4200',
+			'http://192.168.1.138:8081',
+			/.*/
+		],
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
 
-	await app.listen(process.env.PORT || 4200)
+	await app.listen(process.env.PORT || 4200, '0.0.0.0')
 }
 bootstrap()
